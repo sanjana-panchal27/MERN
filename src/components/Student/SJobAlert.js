13a90a98a94ec6de9e5c475ddcd589fb3bd9e952
Hackathon-2023 <sanjanapanchal2713@gmail.com>
2025-02-21 13:49:33 +0530

    add content in job alert

--- src/components/Student/SJobAlert.js
+++ src/components/Student/SJobAlert.js
@@ -5,6 +5,23 @@ const SJobAlert = () => {
     <>
       <div className="stu-container">
         <h1>Job Alert</h1>
+
+        <div className="s-job-alert">
+          <h2>Frontend Developer Intern</h2>
+          <p>ABC Tech</p>
+          <p>Remote</p>
+          <p>Deadline: Feb 25,2025</p>
+
+          <h2>MERN stack developer</h2>
+          <p>Tech</p>
+          <p>Internship</p>
+          <p>Deadline: March 2,2025</p>
+
+          <h2>Full stack developer</h2>
+          <p>ABC Tech</p>
+          <p>Full time</p>
+          <p>Deadline: March 14,2025</p>
+        </div>
       </div>
     </>
   );
