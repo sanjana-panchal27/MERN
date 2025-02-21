903f6ba8b1427d03cbb4dd5668bea349d0aa7e47
Hackathon-2023 <sanjanapanchal2713@gmail.com>
2025-02-21 14:23:21 +0530

    add content in notification

--- src/components/Student/SNotification.js
+++ src/components/Student/SNotification.js
@@ -5,6 +5,25 @@ const SNotification = () => {
     <>
       <div className="stu-container">
         <h1>Notification</h1>
+
+        <div className="s-notification">
+          <h2>Upcoming Placement Drive - Google</h2>
+          <p>
+            Google is conducting a placement drive on March 10.
+            <strong>Register Now!</strong>
+          </p>
+          <p>Feb 20, 2025</p>
+          <h3 className="notification-btn">Placement Drive</h3>
+          <button className="save-btn">Save</button>
+
+          <h2>Resume Submission Deadline</h2>
+          <p>
+            Submit your updated resume by Feb 25 for TCS Recruitment.
+            <strong>Register Now!</strong>
+          </p>
+          <p>Feb 22, 2025</p>
+          <h3 className="notification-btn">Deadline Reminder</h3>
+        </div>
       </div>
     </>
   );
