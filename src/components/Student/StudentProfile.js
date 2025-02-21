7a3d105f2f95f0d9ec33120b545baefdbdd30ac3
Hackathon-2023 <sanjanapanchal2713@gmail.com>
2025-02-21 13:30:01 +0530

    commit changes in create profile

--- src/components/Student/StudentProfile.js
+++ src/components/Student/StudentProfile.js
@@ -75,14 +75,143 @@ const StudentProfile = () => {
 
             <button className="form-sec-btn">Next</button>
           </div>
+          <div className="s-education-sec">
+            <h2>Education Information</h2>
 
-          <h2>Education Information</h2>
-          <input
-            type="text"
-            className="education"
-            placeholder="Degree"
-            required
-          />
+            <label>
+              Degree
+              <input
+                type="text"
+                className="dept"
+                placeholder="Degree"
+                required
+              />
+            </label>
+
+            <label>
+              Board
+              <input
+                type="text"
+                className="dept"
+                placeholder=" Board"
+                required
+              />
+            </label>
+
+            <label>
+              School Name
+              <input
+                type="text"
+                className="dept"
+                placeholder=" School Name"
+                required
+              />
+            </label>
+
+            <label>
+              Passing Month
+              <input
+                type="text"
+                className="dept"
+                placeholder="Passing Month"
+                required
+              />
+            </label>
+
+            <label>
+              Passing Year
+              <input
+                type="text"
+                className="dept"
+                placeholder="  Passing Year"
+                required
+              />
+            </label>
+
+            <label>
+              Seat No.
+              <input
+                type="text"
+                className="dept"
+                placeholder="Seat No."
+                required
+              />
+            </label>
+
+            <label>
+              Passed from State
+              <input
+                type="text"
+                className="dept"
+                placeholder="Passed from State"
+                required
+              />
+            </label>
+
+            <label>
+              Passed from District
+              <input
+                type="text"
+                className="dept"
+                placeholder="Passed from District"
+                required
+              />
+            </label>
+
+            <label>
+              Place of study(City/Town)
+              <input
+                type="text"
+                className="dept"
+                placeholder="Place of study"
+                required
+              />
+            </label>
+
+            <label>
+              Medium of Instruction
+              <input
+                type="text"
+                className="dept"
+                placeholder=" Medium"
+                required
+              />
+            </label>
+
+            <label>
+              Total Marks(overall)
+              <input
+                type="text"
+                className="dept"
+                placeholder="Total Marks"
+                required
+              />
+            </label>
+
+            <label>
+              Obtained Marks(overall)
+              <input
+                type="text"
+                className="dept"
+                placeholder=" Obtained Marks"
+                required
+              />
+            </label>
+
+            <label>
+              Percentage
+              <input
+                type="text"
+                className="dept"
+                placeholder="Percentage"
+                required
+              />
+            </label>
+
+            <p>Upload Degree Certificate</p>
+
+            {/* resume upload */}
+          </div>
         </div>
       </div>
     </>
