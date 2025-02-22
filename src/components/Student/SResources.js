155785ffabaae608a843a7024890f3bae91edf77
Hackathon-2023 <sanjanapanchal2713@gmail.com>
2025-02-22 12:17:41 +0530

    add css in resources

--- src/components/Student/SResources.js
+++ src/components/Student/SResources.js
@@ -12,33 +12,63 @@ const SResources = () => {
         <h1>Resources & Guidance</h1>
 
         <div className="s-resources">
-          <h2 className="grid-1">Resume Building Tips</h2>
-          <p>Guidelines and samples to create a strong Resume</p>
-          <FaBook className="s-resource-icon" />
+          <div className="s-grid-resource">
+            <div className="s-grid-box1">
+              <h2 className="grid-1">Resume Building Tips</h2>
+              <div className="s-icon-text">
+                <FaBook className="s-resource-icon" />
+                <p>Guidelines and samples to create a strong Resume</p>
+              </div>
+            </div>
 
-          <h2 className="grid-1">Interview Preparation</h2>
-          <p>Common interview questions and best practices.</p>
-          <FaBriefcase className="s-resource-icon" />
+            <div className="s-grid-box2">
+              <h2 className="grid-2">Interview Preparation</h2>
+              <div className="s-icon-text">
+                <FaBriefcase className="s-resource-icon" />
+                <p>Common interview questions and best practices.</p>
+              </div>
+            </div>
 
-          <h2 className="grid-1">Aptitude & Technical Tests</h2>
-          <p>Practise tests and study resources for Placements.</p>
-          <FaLaptopCode className="s-resource-icon" />
+            <div className="s-grid-box3">
+              <h2 className="grid-3">Aptitude & Technical Tests</h2>
+              <div className="s-icon-text">
+                <FaLaptopCode className="s-resource-icon" />
+                <p>Practise tests and study resources for Placements.</p>
+              </div>
+            </div>
 
-          <h2>Internship & Job Portals</h2>
-          <p>Useful links to apply for internships and Jobs.</p>
-          <FaBriefcase className="s-resource-icon" />
+            <div className="s-grid-box4">
+              <h2>Internship & Job Portals</h2>
+              <div className="s-icon-text">
+                <FaBriefcase className="s-resource-icon" />
+                <p>Useful links to apply for internships and Jobs.</p>
+              </div>
+            </div>
 
-          <h2>Career Guidance</h2>
-          <p>Expert advice and articles for career growth.</p>
-          <FaChalkboardUser className="s-resource-icon" />
+            <div className="s-grid-box5">
+              <h2>Career Guidance</h2>
+              <div className="s-icon-text">
+                <FaChalkboardUser className="s-resource-icon" />
+                <p>Expert advice and articles for career growth.</p>
+              </div>
+            </div>
 
-          <h2>Webinars & Workshops</h2>
-          <p>Upcoming sessions and recordings from past events.</p>
-          <FaVideo className="s-resource-icon" />
+            <div className="s-grid-box6">
+              <h2>Webinars & Workshops</h2>
+              <div className="s-icon-text">
+                <FaVideo className="s-resource-icon" />
+                <p>Upcoming sessions and recordings from past events.</p>
+              </div>
+            </div>
 
-          <h2>Company Insights</h2>
-          <p>Information about recruiting companies and job roles.</p>
-          <FaBuilding className="s-resource-icon" />
+            <div className="s-grid-box7">
+              <h2>Company Insights</h2>
+              <div className="s-icon-text">
+                <FaBuilding className="s-resource-icon" />
+                <p>Information about recruiting companies and job roles.</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
