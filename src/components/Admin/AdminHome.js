2fb523332bc85d17c384508c2f54ee63d274218f
Hackathon-2023 <sanjanapanchal2713@gmail.com>
2025-02-23 19:34:06 +0530

    changes in admin dashboard

--- src/components/Admin/AdminHome.js
+++ src/components/Admin/AdminHome.js
@@ -4,7 +4,29 @@ const AdminHome = () => {
   return (
     <>
       <div className="admin-container">
-        <h1>Admin Home</h1>
+        <div className="a-grid-container">
+          <div className="a-grid-box blue">
+            <h2 className="a-grid-title">Total Students</h2>
+            <p className="a-grid-p"> 51</p>
+          </div>
+
+          <div className="a-grid-box yellow">
+            <h2 className="a-grid-title">Total Companies</h2>
+            <p className="a-grid-p"> 50 </p>
+          </div>
+
+          {/* job offere recieved */}
+          <div className="a-grid-box green">
+            <h2 className="a-grid-title">Placed Students</h2>
+            <p className="a-grid-p"> 11 </p>
+          </div>
+
+          {/* shortlisted application */}
+          <div className="a-grid-box purple">
+            <h2 className="a-grid-title">Placement Rate</h2>
+            <p className="a-grid-p"> 3%</p>
+          </div>
+        </div>
       </div>
     </>
   );
