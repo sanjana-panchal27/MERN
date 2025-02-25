968c6624484046254da9ca036222f43220e2e43d
Parth Panchal <parthspanchal@192.168.1.5>
2025-02-25 19:43:56 +0530

    Fixed nested repository issue

--- src/App.js
+++ src/App.js
@@ -1,31 +1,30 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar/Navbar";
-import Home from "./components/Navbar/Home";
-import AboutUs from "./components/Navbar/AboutUs";
-import Service from "./components/Navbar/Service";
-import Contact from "./components/Navbar/Contact";
-import FAQs from "./components/Navbar/FAQs";
-import Login from "./components/Login/Login";
-import Footer from "./components/Navbar/Footer";
+import ComNav from "./components/Company/ComNav";
+import ComSidebar from "./components/Company/ComSidebar";
+import CompanyHome from "./components/Company/CompanyHome";
+import CompanyProfile from "./components/Company/CompanyProfile";
+import CPostJob from "./components/Company/CPostJob";
+import CAccess from "./components/Company/CAccess";
+import CShortlist from "./components/Company/CShortlist";
+import CFeedback from "./components/Company/CFeedback";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="app-container">
-        <Navbar />
-        <div className="page-content">
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route exact path="aboutus" element={<AboutUs />} />
-            <Route exact path="service" element={<Service />} />
-            <Route exact path="contact" element={<Contact />} />
-            <Route exact path="faqs" element={<FAQs />} />
-            <Route exact path="login" element={<Login />}></Route>
-          </Routes>
-        </div>
-        <Footer />
+     <div className="company-profile">
+        <ComNav />
+        <ComSidebar />
+
+        <Routes>
+          <Route exact path="/" element={<CompanyHome />}></Route>
+          <Route exact path="/c-profile" element={<CompanyProfile />}></Route>
+          <Route exact path="/c-postjob" element={<CPostJob />}></Route>
+          <Route exact path="/c-access" element={<CAccess />}></Route>
+          <Route exact path="/c-shortlist" element={<CShortlist />}></Route>
+          <Route exact path="/c-feedback" element={<CFeedback />}></Route>
+        </Routes>
       </div>
     </BrowserRouter>
   );
