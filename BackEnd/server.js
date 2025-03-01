9db9d6e65b4d01504d6fcbe1c8175d1aaeb39578
Parth Panchal <parthspanchal@192.168.1.2>
2025-03-01 15:57:52 +0530

    used dynamic routing in backend folder, using express

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -6,3 +6,17 @@
 
 // server.listen(3000);
 
+const express = require ("express");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.get("/profile/:username", function(req, res){
+    res.send(`welcome ${req.params.username} `);
+})
+
+
+app.listen(3000)
